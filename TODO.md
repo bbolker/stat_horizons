d829020939cae894f20a1416432fca8b05db95ef
Ben Bolker <bbolker@gmail.com>
2025-10-29 21:41:40 -0400

    working on 'lecture 2' (example)

--- TODO.md
+++ TODO.md
@@ -3,9 +3,14 @@
 * set up workstation in HH 
    * wired ethernet
    * webcam 
-   
+
+## lecture 1: to add
+
+* maximal models
+
 ## lecture 2: workflow
 
+* add: full model from paper
 * work through workflow with (?) fire data set
 * exploratory (ggplot; tinyplot) [plotnine, seaborn; gadfly, AlgebraOfGraphics.jl
 * fitting. compare glmmTMB, lme4 (gamm4), buildmer ...
@@ -18,4 +23,3 @@
 * structured covariances (splines etc etc etc)
 * inference
 
-https://www.amazon.com/HyperX-SoloCast-Microphone-Tap-Mute/dp/B08KFL3SFV/ref=sr_1_2?crid=1BMK3402KYZME&dib=eyJ2IjoiMSJ9.Q-OfmSUHtL1w2N_tedVk3f_6MQ5UBdVwCiivo_01qyziKh1z4Awn966sBMyDoDgwBbLvNdjezhHute62sJAC60gp95kVBn-NNekhasgtTcGEH69LVqG5blrA6Qd1hepSeTXol8lMOy3KFSacez9kWpkIqbXEkDmkEUn2kFpQE12UbyyIu2crRROiGzdyovpl7XS8ny3MUjMlcA7djNMD5TBdpMgqXpRyVDmp_ktoXCt1tntr3veZyV4ub2gtL7Vga6TfUAfNV5fkbr8j17RzbKprixDYZb4GFkozfdv_WaiMo6dT1yFMoINCM1CDCxDFSUGcCtRYalna-iyqe36vq4yIntnHhWv9ZxAV0V_4Sbs.ASJffC9drB97DETWM462Bv7UIwP50dPjKTyfv4FhhvA&dib_tag=se&keywords=external%2Bmicrophone%2Bfor%2Blaptop&qid=1735920371&s=musical-instruments&sprefix=external%2Bmicrophone%2Bfor%2Blaptop%2Cmi%2C107&sr=1-2&th=1
--- ecoreg.R
+++ ecoreg.R
@@ -2,12 +2,15 @@ library(ggplot2); theme_set(theme_bw())
 library(lme4)
 zmargin <- theme(panel.spacing=unit(0,"lines"))
 library(ggplot2)
-library(marginaleffects)
-library(emmeans)
-library(effects)
 library(tidyverse)
 library(plotly)
 library(buildmer)
+library(broom.mixed)
+library(bbmle)
+
+library(marginaleffects)
+library(emmeans)
+library(effects)
 
 ## exploration
 
@@ -28,9 +31,73 @@ ggplotly(gg2)
 
 ## full model
 
+form <- mbirds_log ~ (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2 +
+  (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | biome) +
+  (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | flor_realms) +
+  (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | biome_FR)
 
+vars <- c("NPP_log_sc", "Feat_log_sc", "NPP_cv_sc", "Feat_cv_sc")
+all_vars <- paste(vars, collapse = "+")
+
+max_form <- sprintf("(%s)^4 + ((%s)^2 | biome) + ((%s)^2 | flor_realms) + ((%s)^2 | biome_FR)",
+                    all_vars, all_vars, all_vars, all_vars)
+max_form2 <- reformulate(max_form, response = "mbirds_log")
+m5 <- lmer(max_form2, data = dd, control = lmerControl(calc.derivs = FALSE))
+
+
+m1 <- lmer(form, data = dd)
+VarCorr(m1)
+
+m2 <- buildmer(form, data = dd)
+## decides on intercepts only at each stage
+
+m3 <- buildmer(form, data = dd,
+               buildmerControl = buildmerControl(include= ~(NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2))
+
+## For birds and amphibians, the selected model included
+## independent effects of the predictors at the biome/realm
+## interaction level and intercept-only (mean diversity)
+## variation at the biome and realm levels; for mammals,
+## the (independent) predictor effects were included at the
+## realm level, with mean- diversity effects at the biome and
+## biome/realm levels.
+form2 <- mbirds_log ~ (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2 +
+  (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc || biome_FR) +
+  (1 | biome) +
+  (1 | flor_realms)
+
+m4 <- lmer(form2, data = dd)
+mod_list <- list(full=m1, reduced = m2@model, reduced_keepfixed = m3@model, used = m4, crazy = m5)
 
 ## diagnostics
+performance::check_model(m3@model)
+pp1 <- DHARMa::simulateResiduals(m3@model)
+pp2 <- DHARMa::simulateResiduals(m3@model, use.u = TRUE)
+plot(pp2)
+
+library(DHARMa)
+plot(simulateResiduals(m4))
+performance::check_model(m4)
+
+## compare fits (coefficient plots etc.)
+
+tt <-  mod_list |>
+  purrr::map_dfr(tidy, effects = "fixed", .id = "model", conf.int = TRUE) |>
+  filter(term != "(Intercept)")
+
+ggplot(tt, aes(estimate, term, colour = model)) +
+  geom_pointrange(aes(xmin = conf.low, xmax = conf.high),
+                  position = position_dodge(width = 0.5)) +
+  geom_vline(xintercept = 0, lty = 2)
 
 
-## coefficient plots etc.
+plot(allEffects(m3@model))
+
+## skip ????
+AICtab(mod_list)
+cAIC_list <- lapply(mod_list, cAIC)
+cAIC_vec <- sapply(cAIC_list, \(x) x$caic)
+cAIC_vec - min(cAIC_vec)
+
+## run cAIC on full set of fitted models??
+## 'full' and 'crazy' are automatically refitted with a subset ... rabbit hole ...
